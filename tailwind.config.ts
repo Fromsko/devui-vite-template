b735485f17ce23f7ff04efc4892ea5c480aac312
Fromsko <99723642+Fromsko@users.noreply.github.com>
2024-11-02 01:04:39 +0800

    fix: 修复全扫描路径

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,6 +5,7 @@ const tailwindConfig: Config = {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx,vue}",
+    '!./node_modules/**', // 排除 node_modules 目录
   ],
   plugins: [
     daisyui,
